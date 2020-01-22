41a9e0c3113c3bdc0536b95ffc4f6104bc7d3491
srikarprayaga <sprayaga@ucsd.edu>
2020-01-21 18:17:12 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Srikar Prayaga on 1/21/20.
+//  Copyright © 2020 Srikar Prayaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
